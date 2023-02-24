876eaadbbe9816f9054629237ac3d61fa396f056
andidprastyo <2141720046@student.polinema.ac.id>
2023-02-24 10:49:40 +0700

    Praktikum 1

---
+++ routes/web.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::get('/', function () {
+    echo "Selamat Datang";
+});
+
+Route::get('/about', function () {
+    echo "Nama : Andi Dwi Prastyo<br>";
+    echo "NIM  : 2141720046";
+});
+
+Route::get('/article/{id}', function ($id) {
+    echo "Halaman Artikel dengan ID $id";
+});
